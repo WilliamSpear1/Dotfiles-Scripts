cdb9259226bf9cce914fdb65eeff3c43b980e07e
William Spearman <wms19@my.fsu.edu>
2022-05-28 15:12:12 -0400

    Created a Script for displaying a slideshow of images using the feh background tool

---
+++ Scripts/slideshow.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# A Script for a slideshows using the feh background tool ###############################################
+while true; do
+	feh --bg-scale --no-fehbg --randomize ~/Wallpapers/*
+	sleep 600
+done
